9861e73eb86b16290d790e468f3c40af6bd4b0a9
Toran Sahu <toran.sahu@yahoo.com>
2026-01-30 15:26:28 +0530

    pytelemetry: env_prefix and tracer_name to be derived from context/scope name

--- pytelemetry/toran/pytelemetry/v1/instrumentation/context.py
+++ pytelemetry/toran/pytelemetry/v1/instrumentation/context.py
@@ -5,8 +5,10 @@ from opentelemetry import trace
 @dataclass(frozen=True)
 class TelemetryContext:
     name: str
-    env_prefix: str
-    tracer_name: str
 
     def tracer(self):
-        return trace.get_tracer(self.tracer_name)
+        return trace.get_tracer(self.name)
+
+    @property
+    def env_var_name(self) -> str:
+        return f"{self.name.upper().replace('-', '_')}_TELEMETRY_ENABLED"
