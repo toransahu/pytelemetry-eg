d760586b12c0e120475312a411382c02df122749
Toran Sahu <toran.sahu@yahoo.com>
2026-01-30 15:36:32 +0530

    sample_service1: Stop passing env_prefix and tracer_name from TelemetryContext obj; Stop using separate exporter and processor and then another processor for conditionally sending spans based on component name - Rather use single exporter and processor for simple demonstration

--- sample_service1/sample_service1/telemetry.py
+++ sample_service1/sample_service1/telemetry.py
@@ -3,31 +3,20 @@ from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 from opentelemetry.sdk.trace import TracerProvider
 from toran.pytelemetry.v1.instrumentation.context import TelemetryContext
 from toran.pytelemetry.v1.telemetry.exporters import SimpleLogSpanExporter
-from toran.pytelemetry.v1.telemetry.processors import ComponentFilteringSpanProcessor
 
 
 SERVICE_CTX = TelemetryContext(
     name="sample_service1",
-    env_prefix="SAMPLE_SERVICE1",
-    tracer_name="sample_service1",
 )
 
 
 def setup_telemetry():
     provider = TracerProvider()
 
-    # exporter for library spans
-    lib_exporter = SimpleSpanProcessor(
-        SimpleLogSpanExporter("sample_lib1")
+    # exporter and processor for all
+    span_processor = SimpleSpanProcessor(
+        SimpleLogSpanExporter()
     )
-    # exporter for service spans
-    svc_exporter = SimpleSpanProcessor(
-        SimpleLogSpanExporter("sample_service1")
-    )
-    provider.add_span_processor(
-        ComponentFilteringSpanProcessor(lib_exporter, component="sample_lib1")
-    )
-    provider.add_span_processor(
-        ComponentFilteringSpanProcessor(svc_exporter, component="sample_service1")
-    )
+    provider.add_span_processor(span_processor)
+
     trace.set_tracer_provider(provider)
